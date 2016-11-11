e62b1fc51a45351180f5f23f6345af8f1e353268
Rodrigo Luger <rodluger@gmail.com>
2016-11-11 16:17:58 +0000

    added everest1 functionality

--- k2plr/api.py
+++ k2plr/api.py
@@ -1363,3 +1363,84 @@ def K2SC(EPIC, version = 1, clobber = False, sci_campaign = None):
         res.sapflux = f[2].data['FLUX'] + f[2].data['TREND_T'] - np.median(f[2].data['TREND_T'])
         
     return res
+
+class everest(object): 
+    pass
+
+def EVEREST(EPIC, version = 1, clobber = False, sci_campaign = None):
+    '''
+    
+    '''
+    
+    # Local directory 
+    base_dir = os.path.join(KPLR_ROOT, "data", "everest", str(EPIC))
+    
+    # Check for local copies
+    file_exists = False
+    if sci_campaign is None:
+        for c in range(30):
+            filename = "hlsp_everest_k2_llc_%09d-c%02d_kepler_v%.1f_lc.fits" % \
+                       (EPIC, c, version)
+            if os.path.exists(os.path.join(base_dir, filename)):
+                sci_campaign = c
+                file_exists = True
+                break
+    else:
+        filename = "hlsp_everest_k2_llc_%09d-c%02d_kepler_v%.1f_lc.fits" % \
+                   (EPIC, sci_campaign, version)
+        if os.path.exists(os.path.join(base_dir, filename)):
+            file_exists = True
+    
+    # Download the data
+    if clobber or not file_exists:
+      
+      # Get the campaign number
+      if sci_campaign is None:
+        client = API()
+        star = client.k2_star(EPIC)
+        tpf = star.get_target_pixel_files(clobber = clobber)
+        sci_campaign = tpf[0].sci_campaign
+        filename = "hlsp_everest_k2_llc_%09d-c%02d_kepler_v%.1f_lc.fits" % \
+                   (EPIC, sci_campaign, version)
+      
+      # Get the url
+      first_four = int(str(EPIC)[:4])
+      last_five = int(str(EPIC)[-5:])
+      url = "https://archive.stsci.edu/missions/hlsp/everest/"
+      url += "c%02d/%04d00000/%05d/" % (sci_campaign, first_four, last_five)
+      url += filename
+      
+      print(url)
+      
+      # Query the server
+      r = urllib.request.Request(url)
+      handler = urllib.request.urlopen(r)
+      code = handler.getcode()
+      if int(code) != 200:
+          raise APIError(code, url, "")
+      data = handler.read()
+      
+      # Make sure that the root directory exists.
+      try:
+          os.makedirs(base_dir)
+      except os.error:
+          pass
+
+      # Atomically write to disk.
+      f = NamedTemporaryFile("wb", delete=False)
+      f.write(data)
+      f.flush()
+      os.fsync(f.fileno())
+      f.close()
+      shutil.move(f.name, os.path.join(base_dir, filename))
+  
+    # Now open the fits file 
+    res = everest() 
+    res._file = os.path.join(base_dir, filename)
+    with pyfits.open(os.path.join(base_dir, filename)) as f:  
+        res.time = f[1].data['TIME']
+        
+        # Get the data
+        res.flux = f[1].data['FLUX']
+        
+    return res
